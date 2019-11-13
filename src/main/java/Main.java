a6f6f318d53ead5d57a1b64bbfd35a78060499de
Thomas <thomas8@hohnen.dk>
2019-11-13 16:22:15 +0100

    Began on translator class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,15 @@ public class Main {
     public static void main(String[] args) {
         GUI_Field[] fields = new GUI_Field[24];
         for (int i = 0; i < 24; i++){
-            GUI_Street test = new GUI_Street("Test" + i, "SubTest" + i, "describtionTest" + i){};
+           /* GUI_Street test = new GUI_Street("Test" + i, "SubTest" + i, "describtionTest" + i){};
             test.setTitle("Test" + i);
             test.setDescription("describtionTest" + i);
-            fields[i] = test;
+            fields[i] = test;*/
         }
+
+        Translator lib = new Translator();
+
+        System.out.println(lib.text.get("Field1"));
 
         GUI gui = new GUI(fields);
     }
---
+++ src/main/java/Translator.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class Translator {
+
+    HashMap<String, String> text = new HashMap<String, String>();
+
+    public Translator() {
+        this.text.put("Key", "Value");
+        this.text.put("Key2", "Value2");
+        this.text.put("Field1", "Lorem Ipsum...");
+
+
+
+
+
+
+        System.out.println(text);
+    }
+
+    // Add keys and (Country, City)
+
+
+
+
+}
