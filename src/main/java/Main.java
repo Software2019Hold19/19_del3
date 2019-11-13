beeb95ec45b2b70252d4256475d7589adcea377b
Frederik Schrøder Koefoed <frederikkoefoeds@gmail.com>
2019-11-13 13:22:52 +0100

    Changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,19 @@
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Street;
 import gui_main.GUI;
+
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        GUI gui = new GUI();
+        GUI_Field[] fields = new GUI_Field[24];
+        for (int i = 0; i < 24; i++){
+            GUI_Street test = new GUI_Street("Test" + i, "SubTest" + i, "describtionTest" + i){};
+            test.setTitle("Test" + i);
+            test.setDescription("describtionTest" + i);
+            fields[i] = test;
+        }
+
+        GUI gui = new GUI(fields);
     }
 }
