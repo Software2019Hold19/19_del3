b1aaac6dfd254d74bf41d3eaa5df9c0f66a775ef
Thomas <thomas8@hohnen.dk>
2019-11-14 12:07:25 +0100

    Set up Die class, PLayer class and Account class

---
+++ src/main/java/Account.java
@@ -0,0 +1,15 @@
+public class Account {
+    private int balance = 1000;
+
+    public Account(){
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public void addBalance(int val){
+        this.balance += val;
+        if(this.balance < 0){ this.balance = 0; }
+    }
+}
---
+++ src/main/java/Die.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Die {
+    private int sides = 6;
+    private int faceValue;
+
+    public Die(){     //constructor
+    }
+
+    public int roll(){
+        Random random = new Random();
+        faceValue = random.nextInt(sides) + 1;
+        return faceValue;
+    }
+}
