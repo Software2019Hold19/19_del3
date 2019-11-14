18f280c646c7c7b73f7f93576b906daa93c2d5f3
Frederik Schrøder Koefoed <frederikkoefoeds@gmail.com>
2019-11-14 12:36:19 +0100

    minor work on Controller

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,18 +6,7 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        GUI_Field[] fields = new GUI_Field[24];
-        for (int i = 0; i < 24; i++){
-           /* GUI_Street test = new GUI_Street("Test" + i, "SubTest" + i, "describtionTest" + i){};
-            test.setTitle("Test" + i);
-            test.setDescription("describtionTest" + i);
-            fields[i] = test;*/
-        }
+        Controller co = new Controller();
 
-        Translator lib = new Translator();
-
-        System.out.println(lib.text.get("Field1"));
-
-        GUI gui = new GUI(fields);
     }
 }
