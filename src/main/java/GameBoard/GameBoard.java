b7468428ac210f770f4c4bb8e51ec21beef274d5
Dánjal <ddanjal@gmail.com>
2019-11-14 15:07:53 +0100

    Partly initializing Field array in GameBoard class

--- src/main/java/GameBoard/GameBoard.java
+++ src/main/java/GameBoard/GameBoard.java
@@ -14,15 +14,20 @@ public class GameBoard {
 
     public void initGame(){
         board[0] = new StartField(lib.text.get("Start0"));
-        board[1] = new StreetField("Brun1", 1, "brown");
-        board[2] = new StreetField("name", 1, "brown");
-        board[3] = new ChanceField("Chance");
-        board[4] = new StreetField("name", 1, "cyan");
-        board[5] = new StreetField("name", 1, "cyan");
-        board[6] = new VisitingField("På besøg");
-        board[7] = new StreetField("name", 2, "magenta");
-        board[8] = new StreetField("name", 2, "magenta");
-
+        board[1] = new StreetField(lib.text.get("Brun1"), 1, "brown");
+        board[2] = new StreetField(lib.text.get("Brun2"), 1, "brown");
+        board[3] = new ChanceField(lib.text.get("Chance3"));
+        board[4] = new StreetField(lib.text.get("Lyseblå4"), 1, "cyan");
+        board[5] = new StreetField(lib.text.get("Lyseblå5"), 1, "cyan");
+        board[6] = new VisitingField(lib.text.get("PåBesøg6"));
+        board[7] = new StreetField(lib.text.get("Lilla7"), 2, "magenta");
+        board[8] = new StreetField(lib.text.get("Lilla8"), 2, "magenta");
+        board[9] = new ChanceField(lib.text.get("Chance3"));
+        board[10] = new StreetField(lib.text.get("Gul9"), 2, "yellow");
+        board[11] = new StreetField(lib.text.get("Gul10"), 2, "yellow");
+        board[12] = new VisitingField(lib.text.get("GratisParkering11"));
+        board[13] = new StreetField(lib.text.get("Rød12"), 3, "red");
+        board[14] = new StreetField(lib.text.get("Rød13"), 3, "red");
     }
 
     public Field[] getBoard() {
