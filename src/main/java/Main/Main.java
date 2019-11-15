9a1bb4730d5fa5eaed24c9ee33e28aa6ef00d585
Thomas <thomas8@hohnen.dk>
2019-11-15 10:55:24 +0100

    Auto stash before merge of "dev" and "origin/dev"

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,18 +2,27 @@ package Main;
 
 import Main.Controller;
 import gui_fields.GUI_Field;
+import gui_fields.GUI_Ownable;
 import gui_fields.GUI_Street;
 import gui_main.GUI;
 
 import java.io.IOException;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         //Controller co = new Controller();
         GUI gui = new GUI();
-        //gui.getFields()[1].setOwnerName("this name");
+        GUI_Field gui_field = new GUI_Street();
+
 
         for (GUI_Field field : gui.getFields())
             System.out.println(field.toString());
+
     }
+
+
+
+
+
 }
