aa19f41b71d13ced2be86c306f9d4c5bb731e186
Oliver Comprés Poulsen <oli@live.dk>
2019-11-18 11:11:44 +0100

    Added two card to changedeck

--- src/main/java/ChanceDeck/ChanceDeck.java
+++ src/main/java/ChanceDeck/ChanceDeck.java
@@ -6,12 +6,13 @@ package ChanceDeck;
  */
 public class ChanceDeck {
 
-    ChanceCard deck[] = new ChanceCard[20];
+    ChanceCard deck[] = new ChanceCard[2];
     int cardCount = 0;
 
     public ChanceDeck() {
         // TODO: Shuffel deck function
-        
+        deck[0] = new BankChanceCard("Få 2 penge", 2);
+        deck[1] = new BankChanceCard("Du minster 2 kort til banken", -2);
     }
 
     public ChanceCard draw() {
